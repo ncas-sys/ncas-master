bab8122a591de6b6ec5522f90ec6972ab4176789
dom <dom.oconnell@me.com>
2015-08-22 14:37:10 +0000

    add emits for lighting scenes

--- app/services/Control.service.js
+++ app/services/Control.service.js
@@ -44,28 +44,34 @@ function Control(events, comms, connections, fans, DB){
 	Control.events.on('AuditDoorOpened', function(){
 		//so we want to do anything?
 		if(Control.states.domes==0 && Control.states.emo==0 && Control.states.lightingScene==null || Control.states.lightingScene=='LightingScene1'){
-			PerformAction('LightingScene3');
+			PerformAction('LightingScene2');
 		}
 	})
 	
 	
 	Control.events.on('LightingScene1', function(){
 		Control.states.lightingScene = 'LightingScene1';
+		Control.events.emit('UpdateState', {node: "lightingScene", value: "B/O"});
 	})
 	Control.events.on('LightingScene2', function(){
 		Control.states.lightingScene = 'LightingScene2';
+		Control.events.emit('UpdateState', {node: "lightingScene", value: "Walkers"});
 	})
 	Control.events.on('LightingScene3', function(){
 		Control.states.lightingScene = 'LightingScene3';
+		Control.events.emit('UpdateState', {node: "lightingScene", value: "House 1/2"});
 	})
 	Control.events.on('LightingScene4', function(){
 		Control.states.lightingScene = 'LightingScene4';
+		Control.events.emit('UpdateState', {node: "lightingScene", value: "Conf 1"});
 	})
 	Control.events.on('LightingScene5', function(){
 		Control.states.lightingScene = 'LightingScene5';
+		Control.events.emit('UpdateState', {node: "lightingScene", value: "Conf 2"});
 	})
 	Control.events.on('LightingScene6', function(){
 		Control.states.lightingScene = 'LightingScene6';
+		Control.events.emit('UpdateState', {node: "lightingScene", value: "Conf 3"});
 	})
 	
 	
